19b6ed62a8a192f5562c60965a8642cd290501f9
KaBence <kabaly.bence@gmail.com>
2022-11-28 21:38:01 +0100

    DMA4

--- DMA - FourthProgramming - Handin/src/median/Median.java
+++ DMA - FourthProgramming - Handin/src/median/Median.java
@@ -6,11 +6,9 @@ import java.util.*;
 public class Median {
 
     // Add your private fields here
-    PriorityQueue test=new PriorityQueue();
     public void add(int x) {
 
         // Implement your method to add x to the data structure
-        test.add(x);
     }
 
     public int median() {
