6785d68edbe172d40d2c80c4d8165dbcf8b0cda7
KaBence <kabaly.bence@gmail.com>
2022-11-28 19:46:20 +0100

    DMA4

---
+++ DMA - FourthProgramming - Handin/src/maze/Maze.java
@@ -0,0 +1,19 @@
+package maze;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.Stream;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Maze
+{
+  public int shortestPath(char[][] maze)
+  {
+      int height = maze.length;
+      int width = maze[0].length;
+
+      // Implement your path-finding algorithm here!
+      return 0;
+  }
+}
---
+++ DMA - FourthProgramming - Handin/src/median/Median.java
@@ -0,0 +1,21 @@
+package median;
+
+import java.io.*;
+import java.util.*;
+
+public class Median {
+
+    // Add your private fields here
+    PriorityQueue test=new PriorityQueue();
+    public void add(int x) {
+
+        // Implement your method to add x to the data structure
+        test.add(x);
+    }
+
+    public int median() {
+
+        // Implement your method to return the median of the numbers added so far
+        return 0;
+    }
+}
